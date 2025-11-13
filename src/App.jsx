61c1d8064bdab6977d5f52c75192cb7a9e6be4eb
Igli <iglibraho77@gmail.com>
2025-11-13 18:57:49 +0100

    configs

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <div className="text-3xl">
+                Welcome to my 3D Portfolio!
+            </div>
+        </main>
+    )
+}
+export default App
