36934971bf7885412dcb7fc8078eab47513b04a6
Igli <iglibraho77@gmail.com>
2025-11-15 14:47:57 +0100

    add features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import Navbar from "./components/Navbar.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <ShowcaseSection />
+            <FeatureCards />
         </>
     )
 }
